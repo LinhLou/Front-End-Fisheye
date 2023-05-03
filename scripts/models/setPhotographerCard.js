c5d367bf0ae358cdbe73dcda24a069f37d1984af
linh lou <dang.tn.linh@gmail.com>
2023-05-03 11:27:38 +0200

    navigation acceuil-photograph + data from id

--- scripts/models/setPhotographerCard.js
+++ scripts/models/setPhotographerCard.js
@@ -1,30 +1,30 @@
 // creat a card DOM for each photographer
 function setPhotographerCard(data) {
-  const { name,  city, country, tagline, price, portrait } = data;
+  const { name,  id, city, country, tagline, price, portrait } = data;
   const picture = `assets/photographers/${portrait}`;
 
   function setPhotographerCardDOM() {
     // link
     const link = document.createElement('a');
-      const img = document.createElement( 'img' );
+      const img = document.createElement('img');
           img.setAttribute("src", picture);
           img.setAttribute("alt", "");
-      const h2 = document.createElement( 'h2' );
+      const h2 = document.createElement('h2');
           h2.textContent = name;
-    link.setAttribute("href","#");
+    link.setAttribute("href",`../../photographer.html#${id}`);
     link.setAttribute("aria-label",name);
     link.classList.add('photographerCard_link');
     link.appendChild(img);
     link.appendChild(h2);
     // infos
     const divInfos = document.createElement('div');
-      const pLocation = document.createElement( 'p' );
+      const pLocation = document.createElement('p');
           pLocation.classList.add('photographerCard_infos-location');
           pLocation.textContent = `${city}, ${country}`;
-      const pSlogan = document.createElement( 'p' );
+      const pSlogan = document.createElement('p');
           pSlogan.classList.add('photographerCard_infos-logan');
           pSlogan.textContent = tagline;
-      const pPrice = document.createElement( 'p' );
+      const pPrice = document.createElement('p');
           pPrice.classList.add('photographerCard_infos-price');
           pPrice.textContent = `${price}€/jour`;
     divInfos.classList.add('photographerCard_infos');
@@ -32,11 +32,11 @@ function setPhotographerCard(data) {
     divInfos.appendChild(pSlogan);
     divInfos.appendChild(pPrice);
     // card
-    const article = document.createElement( 'article' );
+    const article = document.createElement('article');
     article.classList.add('photographerCard');
     article.appendChild(link);
     article.appendChild(divInfos);
       return (article);
   }
-  return { name,  city, country, tagline, price, picture, setPhotographerCardDOM }
+  return { name,  id, city, country, tagline, price, picture, setPhotographerCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,12 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// get id of photographer
+const {href} = window.location; // get current URL
+const idPhotographe = href.split('#')[1]; // get id
+
+async function photographerData(id){
+  const { photographers, media} = await getPhotographersInfos();
+  const photographe = photographers.filter((photographe)=>photographe.id==id);
+  const medias = media.filter((media)=>media.photographerId==id);
+  return {photographe, medias};
+}
