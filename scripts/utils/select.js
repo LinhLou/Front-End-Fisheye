8899593ad0182beab7cf11d86c22aef109641c01
linh lou <dang.tn.linh@gmail.com>
2023-05-13 16:28:26 +0200

    add accessibility to trie section

--- scripts/utils/select.js
+++ scripts/utils/select.js
@@ -1,21 +1,42 @@
 const btnSelect = document.querySelector('._select-btn');
-  const arrowSelect = document.querySelector('.fa-chevron-down');
-  const itemSelect = document.querySelectorAll('._select-item');
-  const listSelect = document.querySelector('._select-list');
+const arrowSelect = document.querySelector('.fa-chevron-down');
+const itemSelect = document.querySelectorAll('._select-item');
+const listSelect = document.querySelector('._select-list');
+const selectOption =['Popularité','Date','Title'];
   
-  const selectOption =['Popularité','Date','Title'];
-  
-  btnSelect.addEventListener('click',(()=>{ 
-    const indexEle=selectOption.findIndex((ele) =>ele==btnSelect.textContent);
+btnSelect.addEventListener('click',(()=>{ 
+  openSelectMenu();
+}));
+
+itemSelect.forEach((ele)=>{
+  ele.addEventListener('click',((e)=>{
+    selectedOption(e.target);
+    closeSelectMenu();
+  }));
+})
+
+  function openSelectMenu(){
+    // show options
     listSelect.classList.toggle('_select-list--visible');
     arrowSelect.classList.toggle('_select-arrow--up');
-  }));
-  
-  itemSelect.forEach((ele)=>{
-    ele.addEventListener('click',(()=>{
-      listSelect.classList.toggle('_select-list--visible');
-      arrowSelect.classList.toggle('_select-arrow--up');
-      btnSelect.innerHTML=ele.textContent;
-      initMedia();
-    }));
-  })
+    // the menu is open
+    btnSelect.setAttribute('aria-expanded','true');
+  }
+
+  function closeSelectMenu(){
+    listSelect.classList.toggle('_select-list--visible');
+    arrowSelect.classList.toggle('_select-arrow--up');
+    btnSelect.setAttribute('aria-expanded','false');
+  }
+
+  function selectedOption(ele){
+    listSelect.setAttribute('aria-activedescendant',ele.id);
+    ele.classList.add('_select-item--selected');
+    ele.setAttribute('aria-selected','true');
+    [...itemSelect].filter((item)=>item.id!==ele.id).forEach((e)=>{
+      e.classList.remove('_select-item--selected'); // remove all selected style from elements not been selected
+      e.removeAttribute('aria-selected');
+    });
+    btnSelect.innerHTML=ele.textContent;
+    initMedia();
+  }
