212a44e266a007f4814674efbc943c2775bc622b
linh lou <dang.tn.linh@gmail.com>
2023-05-31 15:50:21 +0200

    corrigé path data in getData

--- scripts/models/getData.js
+++ scripts/models/getData.js
@@ -1,7 +1,7 @@
 //  get data from the json document
 // bien retourner le tableau photographers seulement une fois récupéré
 async function getData() {
-  const response = await fetch('../../data/photographers.json'); 
+  const response = await fetch('data/photographers.json'); 
   const photographers = await response.json();
   return photographers;
 }
